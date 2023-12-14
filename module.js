c8644259f148037792227f5fcc362d6a5019f4d2
illdub <illia.dubrovin@dreamhost.com>
2023-12-14 13:59:11 +0200

    fix

--- module.js
+++ module.js
@@ -1,3 +1,3 @@
-const func1 = (arr) => arr.map(int => int * 10)
+const func1 = (arr) => arr.map(int => int + 10)
 
 module.exports = func1
--- module.test.js
+++ module.test.js
@@ -3,7 +3,7 @@ const func1 = require('./module')
 
 test("will pass", () => {
     const arr = func1([1, 2, 3, 4])
-    const exp = [10, 20, 30, 40]
+    const exp = [11, 12, 13, 14]
 
     if (JSON.stringify(arr) == JSON.stringify(exp)) {
         return console.log('success')
